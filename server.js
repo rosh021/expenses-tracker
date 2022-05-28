5bfd3655bcfe17e1401459add142b37b48f471a8
rosh021 <roshanbhattarai021@gmail.com>
2022-05-28 14:32:55 +1000

    making server running in localhost

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, error=>{
-    
-})
+app.get("/", (req, res) => {
+  res.send("Hello from the server");
+});
+
+app.listen(PORT, (error) => {
+  error && console.log(error);
+
+  console.log(`Server is running in http://localhost:${PORT}`);
+});
