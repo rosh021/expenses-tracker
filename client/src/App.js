aa1d191c2f01c6fd7180f6f46b279640b74e36b2
rosh021 <roshanbhattarai021@gmail.com>
2022-05-29 10:43:54 +1000

    working on client layout

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="danger" />
+      <AcUnitIcon />
+    </div>
+  );
+}
+
+export default App;
